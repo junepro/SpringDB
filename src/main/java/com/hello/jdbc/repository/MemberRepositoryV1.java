be291d84277341c90a63599bb85b3a90fdd236b0
June <skyve21@naver.com>
2022-10-16 16:13:56 +0900

    abandon

--- src/main/java/com/hello/jdbc/repository/MemberRepositoryV1.java
+++ src/main/java/com/hello/jdbc/repository/MemberRepositoryV1.java
@@ -1,15 +1,25 @@
 package com.hello.jdbc.repository;
 
-import com.hello.jdbc.connection.DBConnectionUtil;
 import com.hello.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.support.JdbcUtils;
 
+import javax.sql.DataSource;
 import java.sql.*;
 import java.util.NoSuchElementException;
-
+/**
+ * jdbc datasource 사용
+ */
 @Slf4j
-public class MemberRepositoryV0 {
+public class MemberRepositoryV1 {
 
+
+
+    private final DataSource dataSource;
+
+    public MemberRepositoryV1(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
     public Member save(Member member) throws SQLException {
 
         String sql = "insert into member(member_id, money) values(?, ?)";
@@ -34,32 +44,16 @@ public class MemberRepositoryV0 {
     }
     private void close(Connection con, Statement stmt, ResultSet rs) {
 
-        if (rs != null) {
-            try {
-                rs.close();
-            } catch (SQLException e) {
-                log.info("error", e);
-            }
-        }
+        JdbcUtils.closeResultSet(rs);
+        JdbcUtils.closeStatement(stmt);
+        JdbcUtils.closeConnection(con);
 
-        if (stmt != null) {
-            try {
-                stmt.close();
-            } catch (SQLException e) {
-                log.info("error", e);
-            }
-        }
-
-        if (con != null) {
-            try {
-                con.close();
-            } catch (SQLException e) {
-                log.info("error", e);
-            }
-        }
     }
-    private Connection getConnection() {
-        return DBConnectionUtil.getConnection();
+
+    private Connection getConnection() throws SQLException {
+        Connection con = dataSource.getConnection();
+        log.info("get connection={}, class={}", con, con.getClass());
+        return con;
     }
 
     public Member findById(String memberId) throws SQLException {
---
+++ src/test/java/com/hello/jdbc/repository/MemberRepositoryV1Test.java
@@ -0,0 +1,60 @@
+package com.hello.jdbc.repository;
+
+import com.hello.jdbc.domain.Member;
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+import java.util.NoSuchElementException;
+
+import static com.hello.jdbc.connection.ConnectionConst.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@Slf4j
+class MemberRepositoryV1Test {
+
+    MemberRepositoryV1 repository;
+
+    @BeforeEach
+    void beforeEach() {
+        //기본 driverManager
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+//        repository = new MemberRepositoryV1(dataSource);
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPoolName(PASSWORD);
+
+        repository = new MemberRepositoryV1(dataSource);
+
+    }
+
+    @Test
+    void crud() throws SQLException {
+        //save
+        Member member = new Member("memberV0", 10000);
+        repository.save(member);
+        //findById
+        Member findMember = repository.findById(member.getMemberId());
+        log.info("findMember={}", findMember);
+        assertThat(findMember).isEqualTo(member);
+
+        //update: money: 10000 -> 20000
+        repository.update(member.getMemberId(), 20000);
+        Member updatedMember = repository.findById(member.getMemberId());
+        assertThat(updatedMember.getMoney()).isEqualTo(20000);
+
+        //delete
+        repository.delete(member.getMemberId());
+        assertThatThrownBy(() -> repository.findById(member.getMemberId()))
+                .isInstanceOf(NoSuchElementException.class);
+    }
+
+
+
+
+}
+
